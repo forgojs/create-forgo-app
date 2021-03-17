210fc2d37020b4591e2b5bee7381e2fb75a6a154
Jeswin <jeswinpk@agilehead.com>
2021-03-17 13:01:09 +0530

    Fix bug when arg.0 is empty

--- src/index.ts
+++ src/index.ts
@@ -17,59 +17,60 @@ const argv = yargs(process.argv.slice(2))
     template: { type: "string", default: "javascript" },
   }).argv;
 
-const projectName = argv._[0].toString();
+const firstArg = argv._[0] ? argv._[0].toString() : undefined;
 
-if (!projectName) {
+if (!firstArg) {
   console.log("You need to specify the name of your project.");
   process.exit(1);
+} else {
+  const projectName = firstArg;
+  const projectPath = join(process.cwd(), projectName);
+
+  if (existsSync(projectPath)) {
+    console.log(`The directory ${projectName} already exists.`);
+    process.exit(1);
+  }
+
+  console.log(`Creating a new Forgo app in ${projectPath}.`);
+
+  async function run() {
+    const gitUrl =
+      argv.template === "typescript"
+        ? "forgojs/forgo-template-typescript#main"
+        : "forgojs/forgo-template-javascript#main";
+
+    await exec(`npx degit ${gitUrl} ${projectName}`);
+
+    const packageJsonPath = join(projectPath, "package.json");
+
+    const packageJson = readFileSync(packageJsonPath).toString();
+
+    const updatedPackageJson = packageJson.replace(
+      "your-forgo-app-name",
+      projectName
+    );
+
+    writeFileSync(packageJsonPath, updatedPackageJson);
+
+    process.chdir(projectPath);
+
+    let hasError = false;
+
+    const npm = spawn("npm", ["i"]);
+
+    npm.stdout.on("data", (data) => {
+      console.log(data.toString());
+    });
+
+    npm.stderr.on("data", (data) => {
+      hasError = true;
+      console.error(data.toString());
+    });
+
+    npm.on("close", (code) => {
+      console.log(`Created ${projectName}.`);
+    });
+  }
+
+  run();
 }
-
-const projectPath = join(process.cwd(), projectName);
-
-if (existsSync(projectPath)) {
-  console.log(`The directory ${projectName} already exists.`);
-  process.exit(1);
-}
-
-console.log(`Creating a new Forgo app in ${projectPath}.`);
-
-async function run() {
-  const gitUrl =
-    argv.template === "typescript"
-      ? "forgojs/forgo-template-typescript#main"
-      : "forgojs/forgo-template-javascript#main";
-
-  await exec(`npx degit ${gitUrl} ${projectName}`);
-
-  const packageJsonPath = join(projectPath, "package.json");
-
-  const packageJson = readFileSync(packageJsonPath).toString();
-
-  const updatedPackageJson = packageJson.replace(
-    "your-forgo-app-name",
-    projectName
-  );
-
-  writeFileSync(packageJsonPath, updatedPackageJson);
-
-  process.chdir(projectPath);
-
-  let hasError = false;
-
-  const npm = spawn("npm", ["i"]);
-
-  npm.stdout.on("data", (data) => {
-    console.log(data.toString());
-  });
-
-  npm.stderr.on("data", (data) => {
-    hasError = true;
-    console.error(data.toString());
-  });
-
-  npm.on("close", (code) => {
-    console.log(`Created ${projectName}.`);
-  });
-}
-
-run();
